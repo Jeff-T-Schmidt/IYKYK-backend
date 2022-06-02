4ab567f5a47ab8c4572b8e5bc73b8db18deef57c
anoorishad <anoorishad@me.com>
2022-06-02 11:30:47 -0700

    fixing merge conflicts

--- models/Event.js
+++ models/Event.js
@@ -1,7 +1,53 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const bcrypt = require("bcrypt");
-const { extensions } = require('sequelize/types/utils/validator-extras');
+
 
 
 class Event extends Model{}
+
+Event.init ({
+    id:{
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    title:{
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    location:{
+        type:DataTypes.TEXT,
+        allowNull:false
+    },
+    details:{
+        type:DataTypes.TEXT,
+        allowNull:false,
+    },
+    time_stamp:{
+        type:DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+        allowNull:false
+    },
+    start_date:{
+        type:DataTypes.DATEONLY,
+        allowNull:false
+    },
+    end_date:{
+        type:DataTypes.DATEONLY,
+        allowNull:false,
+    },
+    admin_id:{
+        type: DataTypes.INTEGER,
+        references:{
+            model:'user',
+            key:'id'
+        }
+    }
+},
+{
+    sequelize,
+    modelName:'event',
+})
+
+module.exports = Event
